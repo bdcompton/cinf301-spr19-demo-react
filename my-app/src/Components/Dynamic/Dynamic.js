78c03d97a9d90eb17523966479287a1dc29c8ef0
bdcompton <bcompton@stetson.edu>
2019-05-01 22:34:27 -0400

    last minute change to boxes that didnt get commited earlier

--- my-app/src/Components/Dynamic/Dynamic.js
+++ my-app/src/Components/Dynamic/Dynamic.js
@@ -1,64 +1,57 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
-import { Link } from "react-router-dom";
 import Grid from '../Grid/Grid'
 import '../../App.css';
 
 class Dynamic extends Component {
+  //eslint-disable-next-line
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      counter: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    //eslint-disable-next-line
-    constructor(props) {
-      super(props);
-      this.state = {
-          isClicked : false
-      };
+  handleClick() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
 
-      this.handleClick = this.handleClick.bind(this);
-    }
+    console.log(this.state.counter);
+  }
 
 
-    handleClick() {
-        if(this.state.isClicked === false){
-        this.setState ({
-            isClicked: true
-        });
-    }
-    else if (this.state.isClicked === true){
-        this.setState ({
-            isClicked: false
-        });
-    }
-       
-    }
 
-    render() {
-        let items = [];
-        if (this.state.isClicked === true){
-            items.push(
-                <Grid/>
-            );
-            
-            //this.setState({isClicked :false})
-    
-        }
-        return (
-          <div className="App Border">
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossorigin="anonymous"
-            />
-            <header className="App-header">
-            {items}
-              <p>
-              <Button variant="primary" onClick={this.handleClick}>Add a Box</Button>
-              </p>
-              
-            </header>
-          </div>
-        );
-      }
+  render() {
+    console.log("hi");
+    let items = [];
+    for (let i = 0; i < this.state.counter; i++)
+      items.push(
+        <Grid />
+      );
+    //this.setState({isClicked :false})
+
+    console.log(items);
+
+    return (
+      <div className="App Border">
+        <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        <header className="App-header">
+          <div>{items}</div>
+          <p>
+            <Button variant="primary"
+              onClick={() => this.handleClick()}>Add a Box</Button>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Dynamic;
